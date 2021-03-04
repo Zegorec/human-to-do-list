104808e0b8bf1ad29e3f9df390cf923190f7ccdf
Zegorec <gorszeg@gmail.com>
2021-03-04 19:48:29 +0300

    good job

--- toDoList.js
+++ toDoList.js
@@ -1,6 +1,46 @@
 const check = () => console.log('Работает');
 const deleteTarget = (item) => {
-  return item.parentNode.remove();
+  item.parentNode.remove();
+};
+const targetCompleted = (input, tag) => {
+  if (input.checked) {
+    tag.classList.add('targetCompleted');
+  } else {
+    tag.classList.remove('targetCompleted');
+  }
+};
+
+const createTarget = (text) => {
+  // Создание div
+  const createDiv = document.createElement('div');
+  createDiv.classList.add('target');
+  // Создание label
+  const createLabel = document.createElement('label');
+  createDiv.appendChild(createLabel);
+  // Создание checkbox
+  const createInput = document.createElement('input');
+  createInput.classList.add('.checkbox-completed');
+  createInput.type = 'checkbox';
+  createLabel.appendChild(createInput);
+  // Создание p
+  const createP = document.createElement('p');
+  createP.classList.add('target-textContent');
+  createP.textContent = text.value;
+  createInput.addEventListener('change', () =>
+    targetCompleted(createInput, createP)
+  );
+  text.value = '';
+  createLabel.appendChild(createP);
+  // Создание кнопки удаления
+  const createButton = document.createElement('button');
+  createButton.classList.add('button-delteTarget');
+  createButton.type = 'button';
+  createButton.textContent = 'Удалить';
+  createDiv.appendChild(createButton);
+  createButton.addEventListener('click', () => deleteTarget(createButton));
+  // Добавление элемента в список
+  text.placeholder = 'Поле ввода';
+  toDoList.appendChild(createDiv);
 };
 
 // Объявление исходных переменных
@@ -8,51 +48,12 @@ const textTarget = document.querySelector('.input-target');
 const buttonAddTarget = document.querySelector('.button-addTarget');
 const toDoList = document.querySelector('.content-toDoList');
 
-// Функция создания элемента
 const addTarget = () => {
   if (textTarget.value == '') {
     return (textTarget.placeholder = 'Долбаеб, задачу напиши');
   } else {
-    // Создание div
-    const createDiv = document.createElement('div');
-    createDiv.classList.add('target');
-    // Создание label
-    const createLabel = document.createElement('label');
-    createDiv.appendChild(createLabel);
-    // Создание checkbox
-    const createInput = document.createElement('input');
-    createInput.classList.add('.checkbox-completed');
-    createInput.type = 'checkbox';
-    createLabel.appendChild(createInput);
-    // Создание p
-    const createP = document.createElement('p');
-    createP.classList.add('target-textContent');
-    createP.textContent = textTarget.value;
-    // Функция обозначения сделанной задачи
-    const targetCompleted = () => {
-      if (createInput.checked) {
-        createP.classList.add('targetCompleted');
-      } else {
-        createP.classList.remove('targetCompleted');
-      }
-    };
-    createInput.addEventListener('change', targetCompleted);
-    textTarget.value = '';
-    createLabel.appendChild(createP);
-    // Создание кнопки удаления
-    const createButton = document.createElement('button');
-    createButton.classList.add('button-delteTarget');
-    createButton.type = 'button';
-    createButton.textContent = 'Удалить';
-    createDiv.appendChild(createButton);
-
-    createButton.addEventListener('click', () => deleteTarget(createButton));
-    // Добавление элемента в список
-    textTarget.placeholder = 'Поле ввода';
-    return toDoList.appendChild(createDiv);
+    createTarget(textTarget);
   }
 };
 
 buttonAddTarget.addEventListener('click', addTarget);
-
-document.addEventListener('change', check)
